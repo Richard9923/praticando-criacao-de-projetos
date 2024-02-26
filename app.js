ef8d0f90334d89bed827cd0581dbae97af5c3693
Richard <programmingislife23@gmail.com>
2024-02-26 10:59:48 -0300

    finalizando o programa

--- app.js
+++ app.js
@@ -86,3 +86,39 @@ const simbolos = [
   "?",
   "|",
 ];
+function criandoSenha() {
+  let numLetras = prompt("Quantas letras você quer na sua senha? ");
+  let numNumeros = prompt("Quantos números você quer na sua senha?");
+  let numSimbolos = prompt("Quantos simbolos você quer na sua senha?");
+  function adicionandoLetras(quantidadeLetras) {
+    for (let i = 0; i < quantidadeLetras; i++) {
+      let randomNum = Math.floor(Math.random() * letras.length);
+      senha.push(letras[randomNum]);
+    }
+  }
+  function adicionandoNumeros(quantidadeNumeros) {
+    for (let i = 0; i < quantidadeNumeros; i++) {
+      let randomNum = Math.floor(Math.random() * numeros.length);
+      senha.push(numeros[randomNum]);
+    }
+  }
+  function adicionandoSimbolos(quantidadeSimbolos) {
+    let randomNum = Math.floor(Math.random() * simbolos.length);
+    senha.push(simbolos[randomNum]);
+  }
+  function convertendoListaParaString() {
+    for (caracter of senha) {
+      senhaFinal += caracter;
+    }
+  }
+  adicionandoLetras(numLetras);
+  adicionandoNumeros(numNumeros);
+  adicionandoSimbolos(numSimbolos);
+  convertendoListaParaString();
+}
+
+let senha = [];
+let senhaFinal = "";
+criandoSenha();
+console.log(senha);
+console.log(`Senha produzida: ${senhaFinal}`);
